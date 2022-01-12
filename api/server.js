425e345a5ad58283ff109f8349ed6fd8f0e606cc
James_Calvo <15jcalvo@gmail.com>
2022-01-11 21:26:08 -0500

    user GET endpoint added

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const server = express()
+const User = require('./users/model')
+
 server.use(express.json())
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
