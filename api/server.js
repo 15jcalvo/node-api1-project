78175b068bfc61ed71c896847af56b2984a28fdf
James_Calvo <15jcalvo@gmail.com>
2022-01-11 21:29:28 -0500

    GET by id added

--- api/server.js
+++ api/server.js
@@ -14,4 +14,18 @@ server.get('/api/users', async (req, res) => {
     }
 })
 
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).json({ message: 'no user' })
+          } else {
+            res.status(200).json(user)
+          }
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
